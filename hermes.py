dcc495aba6de44f601311e0ae06d4e93409b23fd
Mishal <mishal0404@gmail.com>
2024-02-28 11:51:02 +0530

    added python3.11 capability and firebase imports

--- hermes.py
+++ hermes.py
@@ -1,18 +1,35 @@
 #!/usr/bin/env python
 
 import argparse
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
-def main():
-    parser = argparse.ArgumentParser(description="A simple script with argument parsing")
-    
-    parser.add_argument("-f", "--file", help="File Name", required=True)
-
-    args = parser.parse_args()
-
-    file_path = args.file
-
-    print("File path:", file_path)
+cred = credentials.Certificate("servicekey.json")
+firebase_admin.initialize_app(cred)
 
 
-if __name__ == "__main__":
-    main()
+db = firestore.client()
+
+
+data = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+}
+
+db.collection("hermesdb").add(data)
+
+
+parser = argparse.ArgumentParser(description="A simple script with argument parsing")
+
+parser.add_argument("-f", "--file", help="File Name", required=True)
+
+args = parser.parse_args()
+
+file_path = args.file
+
+print("File path:", file_path, "\n")
+
+f = open(file_path, "r")
+print(f.read())     
