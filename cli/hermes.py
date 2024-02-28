123ef7fe3efa4b424ba85a1a502d78e1dd9860c3
Mishal <mishal0404@gmail.com>
2024-02-28 19:54:23 +0530

    allow multiple files to be uploaded

--- cli/hermes.py
+++ cli/hermes.py
@@ -12,31 +12,73 @@ firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
+language_extensions = {
+    "py": "python",
+    "java": "java",
+    "cpp": "c++",
+    "c": "c",
+    "html": "html",
+    "css": "css",
+    "js": "javascript",
+    "php": "php",
+    "rb": "ruby",
+    "swift": "swift",
+    "kt": "kotlin",
+    "scala": "scala",
+    "pl": "perl",
+    "go": "go",
+    "rust": "rust",
+    "lua": "lua",
+    "ts": "typescript",
+    "r": "r",
+    "sh": "shell",
+    "sql": "sql",
+    "asm": "assembly",
+    "json": "json",
+    "xml": "xml",
+    "yaml": "yaml",
+    "ini": "ini",
+    "md": "markdown",
+    "tex": "tex",
+    "svg": "svg",
+    "csv": "csv",
+    "xls": "excel",
+    "xlsx": "excel",
+    "doc": "word",
+    "docx": "word",
+    "pdf": "pdf"
+}
+
 def delete_all_documents_in_collection():
-    docs = db.collection("hermesdb").stream()
+    f = open("username.txt", "r")
+    username = f.read()
+    docs = db.collection("hermesdb/users/" + username).stream()
     for doc in docs:
         doc.reference.delete()
 
 
-def uploadtofirebase(file_path):
-    filename = file_path.split("/")[-1]
-    print("File path:", file_path, "\n")
+def uploadtofirebase(file_paths):
+    delete_all_documents_in_collection()
+    for file_path in file_paths:
+        filename = file_path.split("/")[-1]
+        print("File path:", file_path, "\n")
 
-    f = open(file_path, "r")
-    contents = f.read()     
-    filetype = file_path.split(".")[-1]
+        f = open(file_path, "r")
+        contents = f.read()     
+        filetype = file_path.split(".")[-1]
 
-    data = {
-        "filename": filename,
-        "content": contents,
-        "filetype": filetype
-    }
-    path = "hermesdb/users/"
-    f = open("username.txt", "r")
-    username = f.read()
-    path = path + username
+        data = {
+            "filename": filename,
+            "content": contents,
+            "filetype": language_extensions[filetype]
+        }
+        path = "hermesdb/users/"
+        f = open("username.txt", "r")
+        username = f.read()
+        path = path + username
 
-    db.collection(path).add(data)
+        db.collection(path).add(data)
+    
     website = "https://hacknite.vercel.app/" + username
     print("Visit ", website)
 
@@ -75,14 +117,14 @@ parser.add_argument("operation", choices=["upload", "run", "set"] ,help="debug")
 group_upload = parser.add_argument_group("Upload to Firebase")
 group_set = parser.add_argument_group("Set the debug command")
 
-group_upload.add_argument("-f", "--file",dest="file_path", help="File Name")
+group_upload.add_argument("-f", "--file",dest="file_paths", nargs="+", help="File Name")
 group_set.add_argument("-s", "--set", dest="set_command", help="run command")
 group_set.add_argument("-u", "--username", dest="username", help="run command")
 
 args = parser.parse_args()
 
-if args.operation=="upload" and args.file_path:
-    uploadtofirebase(args.file_path)
+if args.operation=="upload" and args.file_paths:
+    uploadtofirebase(args.file_paths)
 elif args.operation=="set" and args.set_command:
     setcommand(args.set_command) 
 elif args.operation=="set" and args.username:
