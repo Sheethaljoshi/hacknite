c8dd8e2bef7f5a2f64cab508fedda699d04a9861
Sheethal Joshi <sh33thal24@gmail.com>
2024-02-28 22:13:13 +0530

    modified code editor

--- zaria/src/components/Editor.js
+++ zaria/src/components/Editor.js
@@ -4,8 +4,8 @@ import Editor from '@monaco-editor/react';
 const EditorPage = ({ content,filetype, handlechange }) => {
   return (
     <div className="w-4/5 bg-gray-800 text-gray-100">
-      <div className="p-4 font-bold text-xxl">Editor</div>
-      <div className="p-4">
+      <div className="p-4 pl-8 font-bold text-2xl">Code Editor</div>
+      <div className="pl-8">
         <Editor height="85vh" language={filetype} theme="vs-dark" value={content} onChange={handlechange}/>
       </div>
     </div>
--- zaria/src/components/Sidebar.js
+++ zaria/src/components/Sidebar.js
@@ -1,13 +1,13 @@
 // Sidebar.js
 const Sidebar = ({ files, onFileClick1, onFileClick2 }) => {
   return (
-    <div className="w-1/5 h-screen bg-gray-900 text-gray-100">
-      <div className="p-4">File Explorer</div>
-      <div className="bg-gray-800">
+    <div className="w-1/5 h-screen bg-gray-900 text-gray-100 border-r border-gray-600">
+      <div className="p-4 font-bold text-2xl">File Explorer</div>
+      <div className="bg-gray-900 border-t border-gray-600 ">
         {files.map((file, index) => (
           <button
             key={index}
-            className="block w-full px-4 py-2 text-left text-white hover:bg-gray-700"
+            className="block w-full px-4 py-2 text-left text-white hover:bg-gray-700 border-b border-gray-600 rounded-sm"
             onClick={() => {console.log(file);onFileClick1(file.content);onFileClick2(file.filetype)}}
           >
             {file.filename}
