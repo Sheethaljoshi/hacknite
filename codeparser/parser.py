9caeab9361c492948f525defb4e7434be5f4e765
Mishal <mishal0404@gmail.com>
2024-02-29 08:10:11 +0530

    cors

--- codeparser/parser.py
+++ codeparser/parser.py
@@ -2,7 +2,25 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import ast
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+# Set up CORS
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "*"
+    # Add other origins as needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Authorization", "Content-Type"],
+)
 
 class CodeRequest(BaseModel):
     code: str
