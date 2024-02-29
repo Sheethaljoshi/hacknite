a062d8fd66602b2f4cdb0578ce4129c22aed86bd
Sheethal Joshi <sh33thal24@gmail.com>
2024-02-29 09:35:02 +0530

    fix map error

--- zaria/src/components/Sidebar.js
+++ zaria/src/components/Sidebar.js
@@ -4,7 +4,7 @@ const Sidebar = ({ files, onFileClick1, onFileClick2, onFileClick3, edits }) => 
     <div className="w-1/5 h-screen bg-gray-900 text-gray-100 border-r-2 border-gray-600">
       <div className="p-4 font-bold text-2xl">File Explorer</div>
       <div className="bg-gray-900 border-t border-gray-600 ">
-        {files.map((file, index) => (
+        {files && files.map((file, index) => (
           <button
             key={index}
             className="block w-full px-4 py-2 text-left text-white hover:bg-gray-700 border-b border-gray-600 rounded-sm"
@@ -16,7 +16,7 @@ const Sidebar = ({ files, onFileClick1, onFileClick2, onFileClick3, edits }) => 
       </div>
       <div className="p-4 font-bold text-2xl">Edits</div>
       <div className="bg-gray-900 border-t border-gray-600 ">
-        {edits.map((file, index) => (
+        {edits && edits.map((file, index) => (
           <button
             key={index}
             className="block w-full px-4 py-2 text-left text-white hover:bg-gray-700 border-b border-gray-600 rounded-sm"
